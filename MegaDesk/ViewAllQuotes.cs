d71c8d21257bf1977b445745b14bbe18adcef390
Greenway-Benjamin <benjamingreenway@gmail.com>
2019-10-11 18:19:30 -0600

    Loading quotes into forms
    ViewAllQuotes and SearchQuotes forms now load quote objects from quotes.json, and give an error if it fails

--- MegaDesk/ViewAllQuotes.cs
+++ MegaDesk/ViewAllQuotes.cs
@@ -7,15 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace MegaDesk
 {
     public partial class ViewAllQuotes : Form
     {
         MainMenu allQuotesToMenu;
+        List<DeskQuote> list;
         public ViewAllQuotes()
         {
             InitializeComponent();
+
+            //Try to load deskquotes into list, otherwise give error message after form is loaded
+            try
+            {
+                list = getDeskQuotes();
+                //Test quotes
+                //MessageBox.Show(list.ToString(), "Test");
+            }
+            catch
+            {
+                list = new List<DeskQuote>();
+                this.Shown += new EventHandler(ViewAllQuotes_Shown);
+            }
+        }
+
+        void ViewAllQuotes_Shown(object sender, EventArgs e)
+        {
+            //Display Error and return to main menu
+            MessageBox.Show("Something went wrong. Please try again later.", "Error loading quotes");
+            viewAllMainMenu.PerformClick();
         }
 
         private void mainMenu_Click(object sender, EventArgs e)
@@ -26,6 +49,30 @@ namespace MegaDesk
             }
             Hide();
             allQuotesToMenu.Show();
+        }
+
+        private List<DeskQuote> getDeskQuotes()
+        {
+
+            //Create new streamreader, have it read all objects in quotes.json
+            StreamReader reader = new StreamReader("../../Resources/quotes.json");
+            string jsonString = "";
+            while (reader.EndOfStream == false)
+            {
+                jsonString += reader.ReadLine();
+            }
+            reader.Close();
+
+            //Set up list of DeskQuote objects and deserialize json file, return list
+            //If no quotes exist, return empty list
+
+            List<DeskQuote> returnList;
+            returnList = JsonConvert.DeserializeObject<List<DeskQuote>>(jsonString);
+            if (returnList == null)
+            {
+                returnList = new List<DeskQuote>();
+            }
+            return returnList;
         }
     }
 }
