5771feccb1e23b79bae1f1610dde59b48771eac2
bryguy82 <and18017@byui.edu>
2019-11-09 20:24:14 -0300

    Finishing touches on styling

--- MegaDeskWebApp/MegaDeskWebApp/wwwroot/js/site.js
+++ MegaDeskWebApp/MegaDeskWebApp/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function getMaterial(materialValue) {
+
+    pine = "/images/pine.jpg";
+    laminate = "/images/laminate.jpg";
+    oak = "/images/oak.jpg";
+    rosewood = "/images/rosewood.jpg";
+    veneer = "/images/veneer.jpg";
+
+    if (materialValue == "Pine") {
+        document.getElementById("materialImage").src = pine;
+    }
+
+    if (materialValue == "Laminate") {
+        document.getElementById("materialImage").src = laminate;
+    }
+
+    if (materialValue == "Oak") {
+        document.getElementById("materialImage").src = oak;
+    }
+
+    if (materialValue == "Rosewood") {
+        document.getElementById("materialImage").src = rosewood;
+    }
+
+    if (materialValue == "Veneer") {
+        document.getElementById("materialImage").src = veneer;
+    }
+}
